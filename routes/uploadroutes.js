1a37538d0521fd7c9d1ee8fe1fe89521daaf4c8d
QuangMinh07 <jjjminh123jjj@gmail.com>
2024-09-13 02:16:40 +0700

    upload image

---
+++ routes/uploadroutes.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const { uploadFile } = require('../controller/UploadImage'); 
+const { authMiddleware } = require('../middlewares/authMiddleware');
+const router = express.Router();
+
+router.post('/upload', authMiddleware,uploadFile.single('image'), async (req, res) => {
+  try {
+    if (!req.file) {
+      return res.status(400).json({ success: false, error: 'Không có ảnh được tải lên.' });
+    }
+    return res.status(200).json({ success: true, message: 'Tải lên ảnh thành công.', imageUrl: req.file.path });
+  } catch (error) {
+    console.error('Lỗi:', error);
+    return res.status(500).json({ success: false, error: 'Lỗi trong quá trình tải lên.' });
+  }
+});
+router.post('/uploads', uploadFile.array('images', 20), async (req, res) => {
+  try {
+    if (!req.files || req.files.length === 0) {
+      return res.status(400).json({ success: false, error: 'Không có ảnh được tải lên.' });
+    }
+    const imageUrls = req.files.map((file) => file.path);
+    return res.status(200).json({ success: true, message: 'Tải lên ảnh thành công.', imageUrls });
+  } catch (error) {
+    console.error('Lỗi:', error);
+    return res.status(500).json({ success: false, error: 'Lỗi trong quá trình tải lên.' });
+  }
+});
+module.exports = router;
