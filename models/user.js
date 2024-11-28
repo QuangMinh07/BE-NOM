4ee30bba7a4b831dfbf51bd840b12f6a5363aa06
QuangMinh07 <jjjminh123jjj@gmail.com>
2024-11-28 16:07:51 +0700

    update

--- models/user.js
+++ models/user.js
@@ -131,10 +131,10 @@ const UserSchema = new mongoose.Schema(
       type: Date,
       default: null, // Đặt là null khi không có yêu cầu cần duyệt
     },
-    // expoPushToken: {
-    //   type: String,
-    //   default: "", // Khởi tạo với giá trị rỗng
-    // },
+    expoPushToken: {
+      type: String,
+      default: "", // Khởi tạo với giá trị rỗng
+    },
     previousUserName: {
       type: String,
       default: null, // Lưu `userName` trước đó (nếu có)
